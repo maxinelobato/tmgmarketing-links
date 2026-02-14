75d5669604df2ac170e4f36480c12c8ab923cd15
maxinelobato <limabarreto.rg@gmail.com>
2026-02-13 22:02:48 -0300

    change

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.VITE_GTM_ID': JSON.stringify(env.VITE_GTM_ID),
     },
     resolve: {
       alias: {
