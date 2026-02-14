22aecb9b99fb7af5886caaf7c3cab82627031231
maxinelobato <limabarreto.rg@gmail.com>
2026-02-13 21:59:58 -0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', (process as any).cwd())
-
+  const env = loadEnv(mode, '.', '')
   return {
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
+    plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
